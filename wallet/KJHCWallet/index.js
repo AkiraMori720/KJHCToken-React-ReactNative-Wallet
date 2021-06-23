c7b9ae5e9bb0fec8de49a56616c5a5b53f981ebb
kzar1102 <kzar1102@gmail.com>
2021-06-23 12:06:00 +0900

    react-native project basic frame

--- wallet/KJHCWallet/index.js
+++ wallet/KJHCWallet/index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+if (__DEV__) {
+    require('./app/ReactotronConfig');
+} else {
+    console.log = () => {};
+    console.time = () => {};
+    console.timeLog = () => {};
+    console.timeEnd = () => {};
+    console.warn = () => {};
+    console.count = () => {};
+    console.countReset = () => {};
+    console.error = () => {};
+    console.info = () => {};
+}
+
+AppRegistry.registerComponent(appName, () => require('./app/index').default);
